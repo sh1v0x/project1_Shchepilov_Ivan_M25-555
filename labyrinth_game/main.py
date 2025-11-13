2dcc3b7837816ff97b7c9fa5029610fdbb530fd8
sh1v <sh1vvv@yandex.ru>
2025-11-13 19:17:08 +0300

    Выполнен пункт 2.1. Формирование архитектуры и данных

--- labyrinth_game/main.py
+++ labyrinth_game/main.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+from .constants import ROOMS
+from . import player_actions, utils
+
+game_state = {
+    'player_inventory': [], # Инвентарь игрока
+    'current_room': 'entrance', # Текущая комната
+    'game_over': False, # Значения окончания игры
+    'steps_taken': 0 # Количество шагов
+}
+  
+
 def main():
     print('Первая попытка запустить проект!')
 
---
+++ labyrinth_game/utils.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+
+def placeholder() -> None:
+    """Временная заглушка для вспомогательных функций."""
+    return None
+
